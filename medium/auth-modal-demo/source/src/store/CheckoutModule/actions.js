940bd961da94855a7f2aa223783b36e242c3e380
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2019-10-23 23:29:55 +0300

    add to with action

--- medium/auth-modal-demo/source/src/store/CheckoutModule/actions.js
+++ medium/auth-modal-demo/source/src/store/CheckoutModule/actions.js
@@ -22,4 +22,27 @@ export default {
       return res;
     });
   },
+  addProductToWishList({ commit, dispatch, rootState }, payload) {
+    // payload: {
+    //   imageUrl: '',
+    //   name: ''
+    // }
+    if (!rootState.AuthModule.loggedInUser) { // if user not logged in
+      dispatch( // show auth-modal with callbackAction (name, payload)
+        "AuthModule/showAuthModal",
+        {
+          callbackAction: {
+            name: "CheckoutModule/addProductToWishList",
+            payload
+          }
+        },
+        { root: true }
+      );
+      return Promise.resolve();
+    }
+    return Promise.resolve(payload).then(res => { // fake API call
+      commit("UPDATE_WISH_LIST_DATA", payload);
+      return res;
+    });
+  },
 };

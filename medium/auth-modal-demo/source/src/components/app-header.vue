8b7b69620232bb1cb2a193c6c41ef920e59cf33b
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2019-10-23 22:50:54 +0300

    add github repo link in header

--- medium/auth-modal-demo/source/src/components/app-header.vue
+++ medium/auth-modal-demo/source/src/components/app-header.vue
@@ -1,33 +1,37 @@
 <template>
   <b-navbar class="justify-content-between" type="dark" variant="dark">
     <b-navbar-brand href="#">Online Shop</b-navbar-brand>
-
-    <b-dropdown class="dropdown-container" variant="light" right>
-      <template v-slot:button-content>
-        <b-button variant="transparent">
-          <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-          <span class="cart-count">
-            {{ cartLength }}
-          </span>
-        </b-button>
-      </template>
-        <b-card
-          no-body
-          border-variant="transparent"
-          class="cart-item overflow-hidden"
-          v-for="(item, index) in cart"
-          :key="index"
-        >
-          <b-row no-gutters class="align-items-center">
-            <b-col md="4">
-              <b-card-img :src="`https://picsum.photos/100/100/?image=${index}`" class="product-image rounded-0"></b-card-img>
-            </b-col>
-            <b-col md="8">
-              <b-card-body :title="item.name"></b-card-body>
-            </b-col>
-          </b-row>
-        </b-card>
-    </b-dropdown>
+    <div>
+      <b-button class="repo-link" variant="light" href="https://github.com/mahmoudzohdi/zohdi/tree/gh-pages/medium/auth-modal-demo" target="_blank">
+        <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon>
+      </b-button>
+      <b-dropdown class="dropdown-container" variant="light" right>
+        <template v-slot:button-content>
+          <b-button variant="transparent">
+            <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+            <span class="cart-count">
+              {{ cartLength }}
+            </span>
+          </b-button>
+        </template>
+          <b-card
+            no-body
+            border-variant="transparent"
+            class="cart-item overflow-hidden"
+            v-for="(item, index) in cart"
+            :key="index"
+          >
+            <b-row no-gutters class="align-items-center">
+              <b-col md="4">
+                <b-card-img :src="`https://picsum.photos/100/100/?image=${index}`" class="product-image rounded-0"></b-card-img>
+              </b-col>
+              <b-col md="8">
+                <b-card-body :title="item.name"></b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+      </b-dropdown>
+    </div>
   </b-navbar>
 </template>
 
@@ -47,6 +51,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+.repo-link{
+  margin-right: 10px;
+  font-size: 22px;
+}
 .dropdown-menu{
   padding: 15px;
 }
